934bda7aeb69dee85f365cc39db0c0ce4c4304f2
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-07-08 05:14:47 -0400

    feat: impl mint fn

--- src/AdoptAHyphen.sol
+++ src/AdoptAHyphen.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ERC721, ERC721TokenReceiver} from "solmate/tokens/ERC721.sol";
 import {Owned} from "solmate/auth/Owned.sol";
+import {IERC721} from "./interfaces/IERC721.sol";
 import {Base64} from "./utils/Base64.sol";
 import {AdoptAHyphenArt} from "./utils/AdoptAHyphenArt.sol";
 import {AdoptAHyphenMetadata} from "./utils/AdoptAHyphenMetadata.sol";
 
 /// @title AdoptAHyphen
-contract AdoptAHyphen is ERC721, Owned {
-    /// @notice The total number of tokens.
-    uint256 totalSupply;
+contract AdoptAHyphen is ERC721, ERC721TokenReceiver, Owned {
+    /// @notice The Zora NFT contract.
+    IERC721 internal constant ZORA_NFT =
+        IERC721(0x73d24948fD946AE7F20EED63D7C0680eDfaF36f1);
 
     // -------------------------------------------------------------------------
     // Constructor + Mint
@@ -19,11 +21,22 @@ contract AdoptAHyphen is ERC721, Owned {
     /// @param _owner Initial owner of the contract.
     constructor(address _owner) ERC721("adopt-a-friend", "-") Owned(_owner) {}
 
-    /// @notice Mints a token to the sender.
-    function mint() external {
-        unchecked {
-            _mint(msg.sender, ++totalSupply);
-        }
+    /// @notice Mints a token to the sender in exchange for the hyphen NFT from
+    /// Zora (the Zora NFT gets transferred into this contract, and it can never
+    /// be transferred out).
+    /// @dev `msg.sender` must have approvals set to `true` on the Zora NFT with
+    /// the operator as this contract's address.
+    function mint(uint256 _tokenId) external {
+        // Check if the token has been ``burned'' (i.e. transferred into this
+        // contract).
+        require(
+            ZORA_NFT.ownerOf(_tokenId) != address(this),
+            "ERC721: TOKEN_MINTED"
+        );
+        ZORA_NFT.safeTransferFrom(msg.sender, address(this), _tokenId);
+
+        // Mint token.
+        _mint(msg.sender, _tokenId);
     }
 
     // -------------------------------------------------------------------------
