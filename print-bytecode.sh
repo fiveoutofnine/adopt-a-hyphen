7c08e9724015ee37830e756fd014ef9b232df759
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-07-09 02:48:19 -0400

    docs: add docs on deploying/etc.

--- print-bytecode.sh
+++ print-bytecode.sh
@@ -1,6 +1,8 @@
+# Config
+HYPHEN_NFT_CONTRACT_ADDRESS="0x73d24948fD946AE7F20EED63D7C0680eDfaF36f1" # Zora NFT contract
+OWNER="0x08995fAC0a721170E5f2179A1402C786f86535a3" # on-chainparty.eth
+
 BYTECODE=$(forge inspect src/AdoptAHyphen.sol:AdoptAHyphen bytecode)
-# Zora NFT contract: `0x73d24948fD946AE7F20EED63D7C0680eDfaF36f1`
-# Owner: `0x08995fAC0a721170E5f2179A1402C786f86535a3``
-ARGS=$(cast abi-encode "constructor(address,address)" "0x73d24948fD946AE7F20EED63D7C0680eDfaF36f1" "0x08995fAC0a721170E5f2179A1402C786f86535a3")
+ARGS=$(cast abi-encode "constructor(address,address)" $HYPHEN_NFT_CONTRACT_ADDRESS $OWNER)
 CALLDATA=$(cast --concat-hex "$BYTECODE" "$ARGS")
 echo $CALLDATA > bytecode-with-constructor.txt
